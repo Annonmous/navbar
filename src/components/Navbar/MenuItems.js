58435326c4a5e98cfb81c2b890ef8e780459d572
Annonmous <mhuzaifahkhan397@gmail.com>
2021-09-25 15:05:59 -0700

    first commit

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,24 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Services',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About US',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+   
+
+]
